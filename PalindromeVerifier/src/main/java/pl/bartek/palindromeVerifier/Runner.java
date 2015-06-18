6b706016972b34990e642e9b315aae121873502a
kafukafienkov <bartekjmroz@gmail.com>
2015-06-18 19:57:51 +0200

    checking method added

--- PalindromeVerifier/src/main/java/pl/bartek/palindromeVerifier/Runner.java
+++ PalindromeVerifier/src/main/java/pl/bartek/palindromeVerifier/Runner.java
@@ -7,5 +7,10 @@ public class Runner {
 
     public static void main(String[] args) {
 
+        String firstWord = "ajak";
+        String secondWord = "kajak";
+        Verifier verifier = new Verifier();
+        boolean isPalindrome = verifier.isPalindrome(firstWord, secondWord);
+        System.out.println("It is " + isPalindrome + " that the two words together make a palindrome");
     }
 }
--- PalindromeVerifier/src/main/java/pl/bartek/palindromeVerifier/Verifier.java
+++ PalindromeVerifier/src/main/java/pl/bartek/palindromeVerifier/Verifier.java
@@ -1,9 +1,21 @@
 package pl.bartek.palindromeVerifier;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * author: Bartek
  */
 public class Verifier {
 
-    
+    public boolean isPalindrome(String first, String second) {
+        if (StringUtils.isEmpty(first) || StringUtils.isEmpty(second)) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder(second);
+
+        String secondReversed = sb.reverse().toString();
+        return first.equals(secondReversed);
+    }
 }
