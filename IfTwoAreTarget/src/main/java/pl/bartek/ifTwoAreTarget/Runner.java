35faad4b2ff315334c2f2e697b48137528ff5cb9
kafukafienkov <bartekjmroz@gmail.com>
2015-06-19 20:35:20 +0200

    initial commit

---
+++ IfTwoAreTarget/src/main/java/pl/bartek/ifTwoAreTarget/Runner.java
@@ -0,0 +1,11 @@
+package pl.bartek.ifTwoAreTarget;
+
+/**
+ * author: Bartek
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ IfTwoAreTarget/src/main/java/pl/bartek/ifTwoAreTarget/SumCheck.java
@@ -0,0 +1,13 @@
+package pl.bartek.ifTwoAreTarget;
+
+/**
+ * author: Bartek
+ */
+public class SumCheck {
+
+    public int[] twoSum(int[] nums, int target) {
+
+
+        return nums;
+    }
+}
