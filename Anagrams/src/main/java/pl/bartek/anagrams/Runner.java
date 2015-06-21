2a218d49915447b9d0fe06ef4e7eedf02b65cb33
kafukafienkov <bartekjmroz@gmail.com>
2015-06-21 12:10:39 +0200

    solution

--- Anagrams/src/main/java/pl/bartek/anagrams/Runner.java
+++ Anagrams/src/main/java/pl/bartek/anagrams/Runner.java
@@ -1,5 +1,7 @@
 package pl.bartek.anagrams;
 
+import java.util.List;
+
 /**
  * author: Bartek
  */
@@ -7,5 +9,9 @@ public class Runner {
 
     public static void main(String[] args) {
 
+        String[] arrayOfStrings = {"KAJAK", "JAKAK", "BILET", "ROSE", "EROS", "PIKSEL", "EKLIPS", "PLEKSI", "NOGA", "AGON", "SPIEK", "SLIPEK"};
+        Solution solution = new Solution();
+        List<String> foundAnagrams = solution.anagrams(arrayOfStrings);
+        System.out.println(foundAnagrams);
     }
 }
--- Anagrams/src/main/java/pl/bartek/anagrams/Solution.java
+++ Anagrams/src/main/java/pl/bartek/anagrams/Solution.java
@@ -1,7 +1,6 @@
 package pl.bartek.anagrams;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * author: Bartek
@@ -14,6 +13,24 @@ public class Solution {
             return null;
 
         List<String> solution = new ArrayList<String>();
+
+        Map<String, String> mapOfWords = new HashMap<String, String>();
+        for (String checkIfIAmAnagram : strs) {
+            char[] stringToBeSorted = checkIfIAmAnagram.toCharArray();
+            Arrays.sort(stringToBeSorted);
+            String sortedString = new String(stringToBeSorted);
+            if (mapOfWords.containsKey(sortedString)) {
+                String tempAnagram = mapOfWords.get(sortedString);
+                if (tempAnagram.length() == 0 || tempAnagram.charAt(0) != '+') {
+                    solution.add(tempAnagram);
+                    tempAnagram = "+" + tempAnagram;
+                    mapOfWords.put(sortedString, tempAnagram);
+                }
+                solution.add(checkIfIAmAnagram);
+            } else {
+                mapOfWords.put(sortedString, checkIfIAmAnagram);
+            }
+        }
 
         return solution;
     }
