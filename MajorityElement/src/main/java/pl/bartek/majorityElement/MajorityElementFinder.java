3fb6ae04c8501de766ce18b276c59c24b2f766e6
kafukafienkov <bartekjmroz@gmail.com>
2015-06-24 23:17:09 +0200

    solution

--- MajorityElement/src/main/java/pl/bartek/majorityElement/MajorityElementFinder.java
+++ MajorityElement/src/main/java/pl/bartek/majorityElement/MajorityElementFinder.java
@@ -1,9 +1,35 @@
 package pl.bartek.majorityElement;
 
+import java.util.Arrays;
+
 /**
  * author: Bartek
  */
 public class MajorityElementFinder {
 
+    public int majorityElement(int[] nums) {
 
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int half = (nums.length) / 2;
+        int candidate = nums[0];
+        int length = nums.length;
+
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+                if (count > half) {
+                    System.out.println(candidate);
+                    return candidate;
+                }
+            } else {
+                candidate = nums[i];
+                count = 1;
+            }
+        }
+        return 0;
+    }
 }
--- MajorityElement/src/main/java/pl/bartek/majorityElement/Runner.java
+++ MajorityElement/src/main/java/pl/bartek/majorityElement/Runner.java
@@ -7,5 +7,8 @@ public class Runner {
 
     public static void main(String[] args) {
 
+        int[] numbers = {1, 3, 2, 3, 3};
+        MajorityElementFinder finder = new MajorityElementFinder();
+        finder.majorityElement(numbers);
     }
 }
